9916d4ca062e4f25fd21aa4d7369e8cad604da0e
Mark Lynch <malynch7@gmail.com>
2021-02-24 20:38:53 -0500

    UI rework

--- src/ControlPanel.js
+++ src/ControlPanel.js
@@ -8,21 +8,25 @@ export default function ControlPanel({rowCount, isRunning, step, play, pause, ch
     return (
         <div id='control-panel' className='container'>
             <h3 className={'section-header'}>Controls</h3>
-            <div className='button-row'>
-                <button onClick={step}>Step</button>
-                <button onClick={isRunning ? pause : play}>{isRunning ? 'Pause' : 'Play'}</button>
-                <button onClick={resetGrid}>Reset</button>
-                <button onClick={clearGrid}>Clear</button>
+            <div className={'buttons'}>
+                <div className='button-row'>
+                    <button onClick={step}>Step</button>
+                    <button onClick={isRunning ? pause : play}>{isRunning ? 'Pause' : 'Play'}</button>
+                    <button onClick={resetGrid}>Reset</button>
+                    <button onClick={clearGrid}>Clear</button>
+                </div>
+                <div className={'grid-select-wrapper'}>
+                    <label htmlFor='grid-size-select'>Grid Size:</label>
+                    <select value={rowCount.toString()}
+                            id='grid-size-select'
+                            onChange={changeGridDimensions}>
+                        <option value='10'>10 x 10</option>
+                        <option value='25' >25 x 25</option>
+                        <option value='50'>50 x 50</option>
+                        <option value='100'>100 x 100</option>
+                    </select>
+                </div>
             </div>
-            <label htmlFor='grid-size-select'>Grid Size:</label>
-            <select value={rowCount.toString()}
-                    id='grid-size-select'
-                    onChange={changeGridDimensions}>
-                <option value='10'>10 x 10</option>
-                <option value='25' >25 x 25</option>
-                <option value='50'>50 x 50</option>
-                <option value='100'>100 x 100</option>
-            </select>
         </div>
     );
 }
